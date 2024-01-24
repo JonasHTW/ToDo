e5833dde687ab38fd18c3234e783863ed41a76a4
Jonas Pfeiffer <s83836@informatik.htw-dresden.de>
2024-01-24 12:55:07 +0100

    removed unnecessary lines

--- server/src/server.js
+++ server/src/server.js
@@ -8,17 +8,11 @@ const auth = require('./auth');
 const cors = require("cors");
 const initiateScript = require('./initiate/initiate');
 
-// modelle
-const User = require("./dbmodels/user");
-const List = require("./dbmodels/list");
-const Todo = require("./dbmodels/todo");
-
 const app = express();
 const port = 81;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // Verbindung zu MongoDB herstellen
 mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -27,34 +21,14 @@ mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: t
 initiateScript();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/register.html"));
-});
-
-
-app.get('/welcome', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/welcome.html"));
-});
-
-app.get('/find', async (req, res) => {
-
-  // Alle Dokumente in der Benutzer-Sammlung abfragen
-  const alleBenutzer = await User.find({});
-
   res.json({
-    alleBenutzer,
+    message: "Willkommen zur API"
   });
-
 });
-
-//app.post('/login', loginRoute);
-//app.post('/add', JwtAuth, addRoute);
 
 app.use('/auth', auth);
 app.use('/api', api);
 app.use(cors());
-
-// app.post("auth/login", loginRoute);
-
 
 app.listen(port, () => {
   console.log(`listening to http://localhost:${port}`)
